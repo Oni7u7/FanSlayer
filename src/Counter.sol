73a486d9d334c56f15f96597a7c9c7232c0449c0
Oni7u7 <natsugameplay82@gmail.com>
2026-02-21 14:29:16 -0600

    init

---
+++ src/Counter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract FanSlayerArtists is ERC721, Ownable {
+    uint256 private _nextTokenId;
+    uint256 public mintPrice = 0.01 ether;
+    mapping(uint256 => string) public artistName;
+
+    constructor() ERC721("FanSlayer Artists", "FSART") Ownable(msg.sender) {}
+
+    function mintArtist(string calldata _name) external payable {
+        require(msg.value >= mintPrice, "Not enough MON");
+        uint256 tokenId = _nextTokenId++;
+        artistName[tokenId] = _name;
+        _mint(msg.sender, tokenId);
+    }
+
+        function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
